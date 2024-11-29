36989abda13c8fc2e7950430167af97cedbc3d2e
ipquery <admin@ipquery.io>
2024-11-29 10:20:42 -0500

    bumping version number to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ipcore",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python library to query IP addresses using ipquery.io API",
     author="IPQuery",
     author_email="admin@ipquery.io",
