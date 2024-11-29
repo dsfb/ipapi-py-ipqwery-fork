9bd655266582ec70d90d01296aa92784da5a27a5
ipquery <admin@ipquery.io>
2024-11-29 10:33:41 -0500

    Adding a readme for pypi project, and updating version again

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="ipcore",
-    version="0.1.1",
+    version="0.1.2",
     description="A Python library to query IP addresses using ipquery.io API",
     author="IPQuery",
     author_email="admin@ipquery.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ipqwery/ipapi-py",
     packages=find_packages(),
     install_requires=[
